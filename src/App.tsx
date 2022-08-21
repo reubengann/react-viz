d4370f86a32c6b5f1bbf601fc0bba2aa9115df14
Reuben Gann <reubengann@gmail.com>
2022-08-20 23:06:53 -0400

    Basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,47 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { IPopulationService, PopulationDatum } from './services/IPopulationService';
-import { format, scaleBand, scaleLinear } from 'd3'
-import { AxisLeft } from './AxisLeft';
-import { AxisBottom } from './AxisBottom';
-import { Marks } from './Marks';
+import { BrowserRouter, Link, Outlet, Route, Routes } from 'react-router-dom';
+import { PopulationChart } from './pages/PopulationChart';
 
 type AppProps = {
   populationService: IPopulationService
 }
 
 
-function App(props: AppProps) {
-
-  const popService = props.populationService;
-  const [popData, setPopData] = useState(new Array<PopulationDatum>)
-
-  const height = 500;
-  const width = 800;
-  const margin = { top: 20, left: 200, bottom: 60, right: 30 };
-
-  const innerHeight = height - margin.top - margin.bottom;
-  const innerWidth = width - margin.left - margin.right;
-
-  useEffect(() => {
-    const data = popService.getAllPopulationsForYear(2020);
-    setPopData(data.slice(0, 10).map(x => ({ ...x, pop: x.pop * 1000 })));
-  }, []);
-
-  const yScale = scaleBand()
-    .domain(popData.map(d => d.country))
-    .range([0, innerHeight])
-    .padding(0.1)
-
-
-  const xScale = scaleLinear()
-    .domain([0, Math.max(...popData.map(x => x.pop))])
-    .range([0, innerWidth])
-
-
-  const siFormat = format(".2s");
-  function xTickFormatter(n: number) {
-    return siFormat(n).replace('G', 'B');
-  }
-
+const Layout = () => {
   return (
-    <div className="App" >
-      <svg style={{ border: '1px red solid' }} width={width} height={height}>
-        <g transform={`translate(${margin.left},${margin.top})`}>
-          <AxisBottom xScale={xScale} innerHeight={innerHeight} tickFormat={xTickFormatter} />
-          <AxisLeft yScale={yScale} />
-          <Marks xScale={xScale} yScale={yScale}
-            data={popData.map((x) => ({ category: x.country, value: x.pop }))}
-            tooltipFormat={xTickFormatter} />
-          <text className='axis-label' x={innerWidth / 2} y={innerHeight + 45} textAnchor='middle'>Population</text>
-        </g>
-      </svg>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Population</Link>
+          </li>
+          <li>
+            <Link to="/sepal">Sepal</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
   )
+};
+
+function Sepal() {
+  return (<div>sepal</div>)
+}
+
+function App(props: AppProps) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PopulationChart populationService={props.populationService} />} />
+          <Route path="sepal" element={<Sepal />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 
 
 }
